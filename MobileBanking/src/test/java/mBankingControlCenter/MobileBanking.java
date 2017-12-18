b09143aa75026647c177e0139292c3b6db2fc0f4
brantansp <brantansp@FSSCHND13937.fss.india>
2017-12-18 10:21:21 +0530

    initial

---
+++ MobileBanking/src/test/java/mBankingControlCenter/MobileBanking.java
@@ -0,0 +1,56 @@
+package mBankingControlCenter;
+
+import org.testng.annotations.Test;
+import org.apache.commons.logging.*;
+
+import mBankingUtilityCenter.HttpConnect;
+import mBankingUtilityCenter.RequestBuilder;
+import mBankingUtilityCenter.dbTransactionlog;
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class MobileBanking {
+	private static String response;
+	private static int index;
+	private static Log log = LogFactory.getLog(MobileBanking.class);
+    //public static FileHandler fh;  
+	static HttpConnect obj=new HttpConnect();
+	
+	public static void main(String[] args) {
+          /**
+           * For checking Single transactions
+           */
+		index=0; //APBE
+		response =RequestBuilder.buildRequest(index);
+	}
+	
+	@Test
+	public void BalanceEnquiry() throws IOException, SQLException {
+		index=0; //APBE
+		response =RequestBuilder.buildRequest(index);
+		assertTrue(response.contains("BE00"));
+	}
+	
+	@Test
+	public void MiniStatement() throws IOException, SQLException {
+		index=1; //APMS
+		response =RequestBuilder.buildRequest(index);
+		assertTrue(response.contains("MS00"));	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MobileBanking/src/test/java/mBankingUtilityCenter/RequestBuilder.java
@@ -0,0 +1,94 @@
+package mBankingUtilityCenter;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.sql.SQLException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.reporters.jq.Main;
+
+import mBankingConfigurationCenter.Configuration;
+import mBankingConfigurationCenter.StaticStore;
+import mBankingControlCenter.MobileBanking;
+
+public class RequestBuilder {
+static HttpConnect obj=new HttpConnect();
+public static String response="";
+private static String dbResult;
+public static String transactionID = "";
+private static Log log = LogFactory.getLog(RequestBuilder.class);
+
+public static String buildRequest(int index)
+{
+	log.info("******************************START******************************");
+    log.info("Request : "+StaticStore.menuDesc[index][0]);
+    String Request = ""; 
+    if(Configuration.mPINRequired.equals("Y"))
+    {
+    	for(int x=1; x< StaticStore.menuDesc[index].length; x++ )
+    	{	
+    		Request =Request + StaticStore.menuDesc[index][x]+";";
+    	}
+    	} else {
+    		for(int x=1; x< 3; x++ )
+        	{	
+        		Request =Request + StaticStore.menuDesc[index][x]+";"; 
+        	}
+    		for(int y=4; y< StaticStore.menuDesc[index].length; y++ )
+    		{
+    			Request =Request + StaticStore.menuDesc[index][y]+";";   
+    		}
+    	}
+    BigInteger uniNum = RandomNumGenerator.generate();
+	if (Configuration.HMAC.equals("Y"))
+	{
+	  try {
+		Request=Hmac.Hmacing(Request+uniNum, Request, uniNum);
+		log.info("Hmaced Request : "+Request);
+	        } catch (InvalidKeyException | SignatureException | NoSuchAlgorithmException e) {
+		e.printStackTrace();
+	   }
+	}
+	else {
+		Request = Request +uniNum;
+		log.info("Non-Hmac request : "+Request);
+		log.info(StaticStore.menuDesc[index][0]+" Request");
+	}
+	try {
+		response = obj.Post(Request);
+		log.info("Response received from Server : "+response);
+		transactionID= response.substring(response.lastIndexOf("TXNID:")+6, response.lastIndexOf("TXNID:")+18);
+		log.info("Transaction ID : "+transactionID);
+		if(Configuration.dbReport=="Y")
+		{
+			dbResult = dbTransactionlog.fetchRecord(transactionID);
+			System.out.println("dbResult : "+dbResult);
+		}		
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	log.info("******************************END********************************\r\n");
+		return  response;
+  }
+
+public static void main(String[] args) {
+buildRequest(0);
+}
+}
+
+
+
+
+
+
+
+
+
+
