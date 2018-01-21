2c21381a1f845702085aa90eabc375671efdd07d
brantansp <brantansp@brantansp-PC>
2018-01-21 19:52:24 +0530

    Extent reports on multiple classes completed

--- MobileBanking/src/test/java/mBankingControlCenter/NonFinancialTransactions.java
+++ MobileBanking/src/test/java/mBankingControlCenter/NonFinancialTransactions.java
@@ -1,38 +1,25 @@
 package mBankingControlCenter;
 
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+
 import org.testng.annotations.Test;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import mBankingBasePageObject.BaseObject;
+import mBankingUtilityCenter.ExtentManager;
 import mBankingUtilityCenter.HttpConnect;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.sql.SQLException;
 
-public class NonFinancialTransactions{
+public class NonFinancialTransactions extends ExtentManager{
 	private static String response;
 	private static int index;
 	static HttpConnect obj=new HttpConnect();
 
 	public static ExtentReports extent;
 	public static ExtentTest extentLogger;
-	
-	@BeforeSuite
-	public void beforeSuite()
-	{
-		extent = new ExtentReports (System.getProperty("user.dir") +"/test-output/STMExtentReport.html", true);
-	    extent.loadConfig(new File(System.getProperty("user.dir")+"\\extent-config.xml"));	 		   
-	}
-
 	
 	@Test
 	public void AccountSync() throws IOException, SQLException {
