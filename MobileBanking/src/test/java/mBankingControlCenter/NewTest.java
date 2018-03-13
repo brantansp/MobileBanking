5948f7d18a29a27a547c82cd3f89b26d660cf790
brantansp <brantansp@FSSCHND13937.fss.india>
2018-03-13 19:21:11 +0530

    new impl2

---
+++ MobileBanking/src/test/java/mBankingControlCenter/NewTest.java
@@ -0,0 +1,67 @@
+package mBankingControlCenter;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.testng.annotations.Test;
+
+import mBankingBasePageObject.BaseObject;
+import mBankingPageObjectModel.StaticStore;
+import mBankingUtilityCenter.ExtentManager;
+import mBankingUtilityCenter.HttpConnect;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+public class NewTest extends ExtentManager {
+	static HttpConnect obj=new HttpConnect();
+	public static String response="";
+	public static String request;
+	private static String dbResult[];
+	public static String transactionID = "";
+	public static ExtentReports extent;
+	
+	public static ExtentTest test;
+	
+	@Test
+	public void AppLogin() throws IOException, SQLException {
+		request = StaticStore.appLogin();
+		response =buildReq(request, "App Login");
+		assertResponse(response);
+	}
+	
+	@Test
+	public void balanceEnq()
+	{
+		request = StaticStore.balanceEnq();
+		response = buildReq (request, "Balance Enquiry");
+		assertResponse(response);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
