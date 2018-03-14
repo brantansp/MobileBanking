e4b66d5c4fe2346e13b61818f52f61e7ce39a01c
brantansp <brantansp@FSSCHND13937.fss.india>
2018-03-14 11:00:16 +0530

    Payment test

---
+++ MobileBanking/src/test/java/mBankingControlCenter/PaymentTransactions.java
@@ -0,0 +1,37 @@
+package mBankingControlCenter;
+
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import mBankingBasePageObject.BaseObject;
+import mBankingPageObjectModel.StaticStore;
+import mBankingUtilityCenter.ExtentManager;
+import mBankingUtilityCenter.HttpConnect;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.sql.SQLException;
+
+public class PaymentTransactions extends ExtentManager{
+
+	private static String response;
+	private static int index;
+	public static String request;
+	static HttpConnect obj=new HttpConnect();
+
+	public static ExtentReports extent;
+	public static ExtentTest extentLogger;
+	
+  //Billercategorylist
+	@Test
+	public void Billercategorylist() throws IOException, SQLException {
+		request = StaticStore.Billercategorylist();
+		response =sendReq(request, "Billercategorylist");
+		assertResponse(response);
+	}
+	
+	
+
+
+}
